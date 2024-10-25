f09a0aebc4f6ed09a04d5851581d00f85533e1ba
Farellartt <ibnurafiqka2823@gmail.com>
2024-10-25 23:30:48 +0700

    first commit

--- crud_flutter/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "upn_mahasiswa";
-//Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli
-$mysqli = new mysqli($host, $user, $pass, $db);
-?>
